fc30d421d732572f621551cf0ed0b15d83bb1852
Angel Escobar <aescobar-2017379@kinal.edu.gt>
2023-01-20 11:10:59 -0600

    Segundo commit, se agrego una update

---
+++ READMI.md
@@ -0,0 +1,4 @@
+# Pagina web, prueba de Angel
+
+Esta pagina web es para aprender a usar html, css, js y un poco de git cin github
+Saludos ! :D
--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ function saludar(){
     let numA = document.getElementById('ValorA').value;
     let numB = document.getElementById('ValorB').value;
 
-    let resulatdo = parseInt (numA) + parseInt(numB);
+    let resultado = parseInt (numA) + parseInt(numB);
 
     console.log('Saludos');
-    console.log(resulatdo);
+    console.log(resultado);
 }
 
