67b4c1d272aef99a7a3194f8871403579c980cfd
Angel Escobar <aescobar-2017379@kinal.edu.gt>
2023-01-20 10:42:21 -0600

    V1 webpage

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function saludar(){
+
+    let numA = document.getElementById('ValorA').value;
+    let numB = document.getElementById('ValorB').value;
+
+    let resulatdo = parseInt (numA) + parseInt(numB);
+
+    console.log('Saludos');
+    console.log(resulatdo);
+}
+
